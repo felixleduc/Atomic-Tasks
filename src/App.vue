fd80b295e922da9d440c63015c8ffb01a66cdc23
Felix-Antoine Leduc <feled002@edu.cegepgarneau.ca>
2024-03-25 17:59:37 -0400

    feat: Ajouter le titre et les colonnes de taches

---
+++ src/App.vue
@@ -0,0 +1,10 @@
+<script setup lang="ts">
+import Kanban from './components/Kanban.vue'
+</script>
+
+<template>
+  <Kanban/>
+</template>
+
+<style scoped>
+</style>
---
+++ src/components/Kanban.vue
@@ -0,0 +1,38 @@
+<script setup lang="ts">
+//import { ref } from 'vue'
+//const count = ref(0)
+//defineProps<{ msg: string }>()
+
+const columns = [
+  {
+    name: "Aujourd'hui",
+  },
+  {
+    name: "Demain",
+  },
+  {
+    name: "Cette semaine",
+  },
+  {
+    name: "Ce mois-ci",
+  },
+  {
+    name: "En attente",
+  },
+]
+</script>
+
+<template>
+  <div class="container h-screen">
+    <h1 class="py-6">Kanban board</h1>
+    <div class="flex gap-3 h-5/6">
+      <div v-for="column in columns" class="text-center flex-1 bg-slate-300">
+        <h2 class=" py-4">{{ column.name }}</h2>
+        <img src="../assets/plus-icon.svg" alt="Add a card image" class="w-10 mx-auto">
+      </div>
+    </div>
+  </div>
+</template>
+
+<style scoped>
+</style>
